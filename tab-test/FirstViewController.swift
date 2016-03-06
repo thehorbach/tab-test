5553786c77a933d6f60ea1e7c08960ff84972cb6
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-06 15:27:42 +0000

    Initial Commit

---
+++ tab-test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tab-test
+//
+//  Created by Vyacheslav Horbach on 06/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
